8b5b9b2c9cf9d7370c5ca5cfac76eb0b64afd142
Administrator <loom@gft.com>
2022-05-20 12:55:34 +0200

    JPA

---
+++ src/test/java/com/example/jpa_leonardo/JpaLeonardoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa_leonardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLeonardoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
